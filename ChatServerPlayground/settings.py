68a3cec6b78972ad1890cf843f72d33d26e1d3cd
mitch <mitch@tabian.ca>
2020-09-23 12:07:56 -0700

    first static files issue

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -132,8 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
--- ChatServerPlayground/urls.py
+++ ChatServerPlayground/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from personal.views import (
@@ -32,6 +34,9 @@ urlpatterns = [
     path('register/', register_view, name="register"),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
