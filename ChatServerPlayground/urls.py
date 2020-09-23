4ed8af025feb8639f58d269f32a9c987ccae61cf
mitch <mitch@tabian.ca>
2020-09-23 12:26:16 -0700

    fix static files issue

--- ChatServerPlayground/urls.py
+++ ChatServerPlayground/urls.py
@@ -14,16 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from personal.views import (
-	home_screen_view
+    home_screen_view
 )
 
 urlpatterns = [
-	path('', home_screen_view, name='home'),
-	path('admin/', admin.site.urls),
+    path('', home_screen_view, name='home'),
+    path('admin/', admin.site.urls),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
