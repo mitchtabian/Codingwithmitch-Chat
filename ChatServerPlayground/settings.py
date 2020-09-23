83f6631081c8feacb432db21913b1654abac9bfa
mitch <mitch@tabian.ca>
2020-09-23 12:12:41 -0700

    fix static files issue

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -132,11 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
 BASE_URL = "http://127.0.0.1:8000"
+
