921d26c6cfff041cd41d709444c40ca7d7d31135
mitch <mitch@tabian.ca>
2020-09-28 15:36:08 -0700

    add redis config

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -84,6 +84,15 @@ WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
 
 ASGI_APPLICATION = "ChatServerPlayground.routing.application"
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
