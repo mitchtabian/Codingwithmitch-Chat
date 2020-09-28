d67d4a7d69fa7323f3fa43e33a9fc97e43e74111
mitch <mitch@tabian.ca>
2020-09-28 10:16:59 -0700

    query friend requests

--- friend/urls.py
+++ friend/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from friend.views import(
 	send_friend_request,
+	friend_requests,
 )
 
 app_name = 'friend'
 
 urlpatterns = [
     path('friend_request/', send_friend_request, name='friend-request'),
+    path('friend_requests/<user_id>/', friend_requests, name='friend-requests'),
 ]
--- friend/views.py
+++ friend/views.py
@@ -1,8 +1,25 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 import json
 
 from account.models import Account
 from friend.models import FriendRequest
+
+
+def friend_requests(request, *args, **kwargs):
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		user_id = kwargs.get("user_id")
+		account = Account.objects.get(pk=user_id)
+		if account == user:
+			friend_requests = FriendRequest.objects.filter(receiver=account, is_active=True)
+			context['friend_requests'] = friend_requests
+		else:
+			return HttpResponse("You can't view another users friend requets.")
+	else:
+		redirect("login")
+	return render(request, "friend/friend_requests.html", context)
 
 
 def send_friend_request(request, *args, **kwargs):
