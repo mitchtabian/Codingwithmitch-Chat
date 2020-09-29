134eafae051f5b3769acfe762057f9bcd5dd27ec
mitch <mitch@tabian.ca>
2020-09-29 13:11:49 -0700

    formatting timestamps using django humanize

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -1,6 +1,9 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 from django.contrib.auth import get_user_model
+from django.contrib.humanize.templatetags.humanize import naturaltime, naturalday
+from django.utils import timezone
+from datetime import datetime
 
 User = get_user_model()
 
@@ -75,6 +78,7 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 		"""
 		# Send a message down to the client
 		print("PublicChatConsumer: chat_message from user #" + str(event["user_id"]))
+		timestamp = calculate_timestamp(timezone.now())
 		await self.send_json(
 			{
 				"msg_type": MSG_TYPE_MESSAGE,
@@ -82,6 +86,7 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 				"username": event["username"],
 				"user_id": event["user_id"],
 				"message": event["message"],
+				"natural_timestamp": timestamp,
 			},
 		)
 
@@ -99,7 +104,26 @@ class ClientError(Exception):
 
 
 
-
+def calculate_timestamp(timestamp):
+    """
+    1. Today or yesterday:
+        - EX: 'today at 10:56 AM'
+        - EX: 'yesterday at 5:19 PM'
+    2. other:
+        - EX: 05/06/2020
+        - EX: 12/28/2020
+    """
+    ts = ""
+    # Today or yesterday
+    if (naturalday(timestamp) == "today") or (naturalday(timestamp) == "yesterday"):
+        str_time = datetime.strftime(timestamp, "%I:%M %p")
+        str_time = str_time.strip("0")
+        ts = f"{naturalday(timestamp)} at {str_time}"
+    # other days
+    else:
+        str_time = datetime.strftime(timestamp, "%m/%d/%Y")
+        ts = f"{str_time}"
+    return str(ts)
 
 
 
