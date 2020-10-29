bcf3874d11d2eaea5c19eebf9c0fbae96880ad8c
mitch <mitch@tabian.ca>
2020-10-29 14:37:02 -0700

    general notification payloads

--- notification/consumers.py
+++ notification/consumers.py
@@ -45,7 +45,10 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
 			},
 		)
 
-	
+
+
+
+
 
 
 
---
+++ notification/utils.py
@@ -0,0 +1,46 @@
+from django.core.serializers.python import Serializer
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+
+class LazyNotificationEncoder(Serializer):
+	"""
+	Serialize a Notification into JSON. 
+	There are 3 types
+		1. FriendRequest
+		2. FriendList
+		3. UnreadChatRoomMessage
+	"""
+	def get_dump_object(self, obj):
+		dump_object = {}
+		if obj.get_content_object_type() == "FriendRequest":
+			dump_object.update({'notification_type': obj.get_content_object_type()})
+			dump_object.update({'notification_id': str(obj.pk)})
+			dump_object.update({'verb': obj.verb})
+			dump_object.update({'is_active': str(obj.content_object.is_active)})
+			dump_object.update({'is_read': str(obj.read)})
+			dump_object.update({'natural_timestamp': str(naturaltime(obj.timestamp))})
+			dump_object.update({'timestamp': str(obj.timestamp)})
+			dump_object.update({
+				'actions': {
+					'redirect_url': str(obj.redirect_url),
+				},
+				"from": {
+					"image_url": str(obj.from_user.profile_image.url)
+				}
+			})
+		if obj.get_content_object_type() == "FriendList":
+			dump_object.update({'notification_type': obj.get_content_object_type()})
+			dump_object.update({'notification_id': str(obj.pk)})
+			dump_object.update({'verb': obj.verb})
+			dump_object.update({'natural_timestamp': str(naturaltime(obj.timestamp))})
+			dump_object.update({'is_read': str(obj.read)})
+			dump_object.update({'timestamp': str(obj.timestamp)})
+			dump_object.update({
+				'actions': {
+					'redirect_url': str(obj.redirect_url),
+				},
+				"from": {
+					"image_url": str(obj.from_user.profile_image.url)
+				}
+			})
+		return dump_object
