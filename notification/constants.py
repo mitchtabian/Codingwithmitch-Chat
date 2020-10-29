e95d1abcef9a0847eae26b80616622df11cef4e4
mitch <mitch@tabian.ca>
2020-10-29 14:46:00 -0700

    fetch general notifications

---
+++ notification/constants.py
@@ -0,0 +1,8 @@
+DEFAULT_NOTIFICATION_PAGE_SIZE = 15
+
+"""
+"General" notifications include:
+	1. FriendRequest
+	2. FriendList
+"""
+GENERAL_MSG_TYPE_NOTIFICATIONS_PAYLOAD = 0  # New 'general' notifications data payload incoming
--- notification/consumers.py
+++ notification/consumers.py
@@ -1,5 +1,15 @@
+from django.conf import settings
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from django.core.paginator import Paginator
+from django.core.serializers import serialize
+from channels.db import database_sync_to_async
+from django.contrib.contenttypes.models import ContentType
 
+import json
+
+from friend.models import FriendRequest, FriendList
+from notification.utils import LazyNotificationEncoder
+from notification.constants import *
 
 
 class NotificationConsumer(AsyncJsonWebsocketConsumer):
@@ -35,7 +45,17 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
 		"""
 		command = content.get("command", None)
 		print("NotificationConsumer: receive_json. Command: " + command)
-
+		try:
+			if command == "get_general_notifications":
+				payload = await get_general_notifications(self.scope["user"], content.get("page_number", None))
+				if payload == None:
+					pass
+				else:
+					payload = json.loads(payload)
+					await self.send_general_notifications_payload(payload['notifications'], payload['new_page_number'])
+		except Exception as e:
+			print("EXCEPTION: receive_json: " + str(e))
+			pass
 
 	async def display_progress_bar(self, shouldDisplay):
 		print("NotificationConsumer: display_progress_bar: " + str(shouldDisplay)) 
@@ -45,9 +65,50 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
 			},
 		)
 
+	async def send_general_notifications_payload(self, notifications, new_page_number):
+		"""
+		Called by receive_json when ready to send a json array of the notifications
+		"""
+		#print("NotificationConsumer: send_general_notifications_payload")
+		await self.send_json(
+			{
+				"general_msg_type": GENERAL_MSG_TYPE_NOTIFICATIONS_PAYLOAD,
+				"notifications": notifications,
+				"new_page_number": new_page_number,
+			},
+		)
 
 
 
+@database_sync_to_async
+def get_general_notifications(user, page_number):
+	"""
+	Get General Notifications with Pagination (next page of results).
+	This is for appending to the bottom of the notifications list.
+	General Notifications are:
+	1. FriendRequest
+	2. FriendList
+	"""
+	if user.is_authenticated:
+		friend_request_ct = ContentType.objects.get_for_model(FriendRequest)
+		friend_list_ct = ContentType.objects.get_for_model(FriendList)
+		notifications = Notification.objects.filter(target=user, content_type__in=[friend_request_ct, friend_list_ct]).order_by('-timestamp')
+		p = Paginator(notifications, DEFAULT_NOTIFICATION_PAGE_SIZE)
+
+		payload = {}
+		if len(notifications) > 0:
+			if int(page_number) <= p.num_pages:
+				s = LazyNotificationEncoder()
+				serialized_notifications = s.serialize(p.page(page_number).object_list)
+				payload['notifications'] = serialized_notifications
+				new_page_number = int(page_number) + 1
+				payload['new_page_number'] = new_page_number
+		else:
+			return None
+	else:
+		raise NotificationClientError("User must be authenticated to get notifications.")
+
+	return json.dumps(payload)
 
 
 
