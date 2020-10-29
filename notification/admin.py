d5e468445468b3d9c93628e1d203ccb35043735f
mitch <mitch@tabian.ca>
2020-10-29 12:04:25 -0700

    notification admin

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from notification.models import Notification
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_filter = ['content_type',]
+    list_display = ['target', 'content_type', 'timestamp']
+    search_fields = ['target__username',]
+    readonly_fields = []
+
+    class Meta:
+        model = Notification
+
+
+admin.site.register(Notification, NotificationAdmin)
