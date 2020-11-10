f1d9bb5dd4df36218c62f832fc8a887f2b900e35
mitch <mitch@tabian.ca>
2020-11-10 15:45:08 -0800

    update config

--- CodingWithMitchChat/settings.py
+++ CodingWithMitchChat/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ChatServerPlayground.urls'
+ROOT_URLCONF = f'{config('PROJECT_NAME')}.urls'
 
 TEMPLATES = [
     {
@@ -87,9 +87,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
+WSGI_APPLICATION = f'{config('PROJECT_NAME')}.wsgi.application'
 
-ASGI_APPLICATION = 'ChatServerPlayground.routing.application'
+ASGI_APPLICATION = f'{config('PROJECT_NAME')}.routing.application'
 
 CHANNEL_LAYERS = {
     'default': {
